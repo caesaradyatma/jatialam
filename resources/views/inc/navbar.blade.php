10cc83b747c49784af7998020d581ba930f79e78
farhanfary21 <farhanfary21@yahoo.com>
2018-08-23 01:04:54 +0700

    Navbar Update

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,13 @@
+<div class="sidenav">
+    <button class="dropdown-btn">Inventory 
+        <i class="fa fa-caret-down"></i>
+    </button>
+        <div class="dropdown-container">
+            <a href="#">Tambah Barang</a>
+            <a href="/inventory">Daftar Gudang</a>
+            <a href="#">Pengaturan</a>
+        </div>
+    <a href="#services">Procurement</a>
+    <a href="#clients">Manufacturing</a>
+    <a href="#contact">Delivery</a>
+</div>
