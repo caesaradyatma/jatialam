a1dc75af5171362d5da7578f6419ae5e237c9547
caesaradyatma <caesar_prayogo@hotmail.com>
2018-09-03 22:28:45 +0700

    Navbar Edit

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -13,6 +13,24 @@
             <a href="/purchasings">List Procurement</a>
             <a href="/purchasings/create">Buat Procurement</a>
         </div>
-    <a href="#clients">Manufacturing</a>
+    <button class="dropdown-btn">Manufacturing 
+        <i class="fa fa-caret-down"></i>
+    </button>
+        <div class="dropdown-container">
+            <button class="dropdown-btn">Cutting Stage 
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-container">
+                <a href="/cuttings">List of Processes</a>
+                <a href="/cuttings/create">Create Process</a>
+            </div>
+            <button class="dropdown-btn">Oven Stage 
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-container">
+                <a href="/ovens">List of Processes</a>
+                <a href="/ovens/create">Create Process</a>
+            </div>
+        </div>
     <a href="#contact">Delivery</a>
 </div>
